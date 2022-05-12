98926f6d78ed271e707393566373e02b5db3f097
Manuela França <manuelaalves26@gmail.com>
2022-05-12 15:46:16 -0300

    Implementei a função objPlayground.

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,23 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (num1, num2) => ({
+  sum: num1 + num2,
+  mult: num1 * num2,
+  div: Math.floor(num1 / num2),
+  sub: num1 - num2, 
+});
 
-const arrayGenerator = (type, object) => {};
+function arrayGenerator(type, object) {
+  if (type === 'keys') {
+    return Object.keys(object);
+  }
+  if (type === 'values') {
+    return Object.values(object);
+  }
+  if (type === 'entries') {
+    return Object.entries(object);
+  }
+}
 
 module.exports = { calculator, arrayGenerator };
