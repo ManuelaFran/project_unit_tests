978c4cce43fb68693389e520fcf7fbe07c4fa493
Manuela França <manuelaalves26@gmail.com>
2022-05-12 18:48:38 -0300

    Implementei a função getCharacter.

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const getCharacter = require('../src/getCharacter');
+const {getCharacter, characters} = require('../src/getCharacter');
 
 /*
 A função getCharacter recebe uma string que representa o nome de um personagem e retorna um objeto contendo seu nome, sua classe e suas frases.
@@ -43,13 +43,20 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter()).toBe(undefined);
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Arya')).toBe(characters.arya);
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('Brienne')).toBe(characters.brienne);
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(getCharacter('Melissandre')).toBe(characters.melissandre);
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+    if(getCharacter('ARYA') || getCharacter('ArYa')){
+      expect(getCharacter('arya')).toBe(characters.arya);
+    }
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('Snow')).toBe(undefined);
   });
 });
