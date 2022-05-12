8f349ebba347f46eb90a7a055fb75d9cac751e76
Manuela França <manuelaalves26@gmail.com>
2022-05-12 16:24:39 -0300

    Alterei valores nos testes cinco e seis.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,16 +25,17 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    expect(circle('a')).toBe(undefined);
+    expect(circle('m')).toBe(undefined);
     // Teste se circle retorna um objeto.
     expect(circle(1)).toEqual({radius: 1, area: 3.14, circumference: 6.28});
     // Teste se o objeto retornado possui 3 propriedades.
-    expect(circle(7)).toHaveProperty('radius', 'area', 'circumference');
+    expect(circle(3)).toHaveProperty('radius', 'area', 'circumference');
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     expect(circle()).toBe(undefined);
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
-    expect(circle(3)).toEqual({radius: 3, area: 28.259999999999998, circumference: 18.84});
+    expect(circle(2)).toEqual({radius: 2, area: 12.56, circumference: 12.56});
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
+    expect(circle(3)).toEqual({radius: 3, area: 28.259999999999998, circumference: 18.84});
     // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
   });
 });
