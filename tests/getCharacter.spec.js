e9fdd91fa6176bd0613b65a5b75684712105e57e
Manuela França <manuelaalves26@gmail.com>
2022-05-12 21:50:49 -0300

    Refatorei o requisito 9.

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const {getCharacter, characters} = require('../src/getCharacter');
+const getCharacter = require('../src/getCharacter');
 
 /*
 A função getCharacter recebe uma string que representa o nome de um personagem e retorna um objeto contendo seu nome, sua classe e suas frases.
@@ -45,18 +45,30 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    expect(getCharacter()).toBe(undefined);
+    expect(getCharacter()).toEqual(undefined);
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    expect(getCharacter('arya')).toBe(characters.arya);
+    expect(getCharacter('arya')).toEqual({
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.']
+    });
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    expect(getCharacter('brienne')).toBe(characters.brienne);
+    expect(getCharacter('brienne')).toEqual({
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.']
+    });
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    expect(getCharacter('melissandre')).toBe(characters.melissandre);
+    expect(getCharacter('melissandre')).toEqual({
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.']
+    });
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
-    if(getCharacter('ARYA') || getCharacter('ArYa')){
-      expect(getCharacter('arya')).toBe(characters.arya);
+    if(getCharacter('ARYA') || getCharacter('Arya') || getCharacter('ArYa')){
+      expect(getCharacter('Arya'))
     }
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
-    expect(getCharacter('Snow')).toBe(undefined);
+    expect(getCharacter('Snow')).toEqual(undefined);
   });
 });
